a63f27346e4e48ca3d30b64faf1c3ed5f20bfcee
kikuchi <nyoronyoro0327@gmail.com>
2022-06-13 20:49:11 +0900

    Update ABC155B.py

--- ABC155B.py
+++ ABC155B.py
@@ -1,11 +1,13 @@
 # B - Papers, Please
 # https://atcoder.jp/contests/abc155/tasks/abc155_b
 
+# answerという変数を置いて、1つでも当てはまらないものが出てきたら回答が変化するように設定。
+
 def immigration():
     n = int(input())
     a = list(map(int, input().split()))
     answer = "APPROVED"
-    
+
     for i in a:
         if i % 2 == 0:
             if i % 3 != 0 and i % 5 != 0:
