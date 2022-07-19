bfc1b4c30edf9ff10b9e0ca52c17fc1630f6e0ff
kikuchi <nyoronyoro0327@gmail.com>
2022-07-19 23:41:43 +0900

    Revert "Merge branch 'main' of https://github.com/lily-engine/AtCoder"
    This reverts commit 92e50391e8a4bb34e8dfcf1aa22890e6787bed9e, reversing
    changes made to 1282a91d5fa2989caaf50a2f434caae23b1b5066.

--- test2.py
+++
@@ -1,6 +0,0 @@
-# べき乗は**を使う
-
-sum = 0
-for i in range(1,81):
-    sum += i ** 4
-print(sum)
--- test3.py
+++
@@ -1,9 +0,0 @@
-# 3で割り切れるもの、もしくは数値に3が含まれるもの
-
-sum = 0
-for i in range(1,30001):
-    if i % 3 == 0:
-        sum += i
-    elif '3' in str(i):
-        sum += i
-print(sum)
